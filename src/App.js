9fa4ff7f44c1b175346adce617f7b2416f1e1c96
thermiee <navyblack16@yahoo.com>
2020-01-09 15:57:40 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Pokecard from './Pokecard';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+     <Pokecard />
+    </div>
+  );
+}
+
+export default App;
