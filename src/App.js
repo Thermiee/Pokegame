3a370adcb90dbfd1ca562239f300406ede9868be
thermiee <navyblack16@yahoo.com>
2020-01-10 16:05:12 +0100

    created first Pokecard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Pokecard from './Pokecard';
 import './App.css'
 
-function App() {
+class App extends Component{
+  render () {
   return (
     <div className="App">
-     <Pokecard />
+     <Pokecard id={4} name="Charmander" type="fire" exp={62} />
     </div>
   );
+  }
 }
 
 export default App;
