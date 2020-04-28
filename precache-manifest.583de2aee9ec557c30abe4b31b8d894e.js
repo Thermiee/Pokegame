9cf12399a0341708ddb1a03fa84d03b7021116f2
themiee <navyblack16@yahoo.com>
2020-04-28 08:30:56 +0100

    Updates

---
+++ precache-manifest.583de2aee9ec557c30abe4b31b8d894e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca185937f5c0cb36f67428f0bcf2815f",
+    "url": "/Pokegame/index.html"
+  },
+  {
+    "revision": "f5a9307b3bc660f517b1",
+    "url": "/Pokegame/static/css/main.5493e5a9.chunk.css"
+  },
+  {
+    "revision": "7ddec355faec310b5d13",
+    "url": "/Pokegame/static/js/2.c0f23797.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Pokegame/static/js/2.c0f23797.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5a9307b3bc660f517b1",
+    "url": "/Pokegame/static/js/main.3b3b644c.chunk.js"
+  },
+  {
+    "revision": "495bf9e098d33a957840",
+    "url": "/Pokegame/static/js/runtime-main.1873644f.js"
+  }
+]);
